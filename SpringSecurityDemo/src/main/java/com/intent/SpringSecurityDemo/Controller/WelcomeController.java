9a7f2d26f0d43e08503a2d13f67157ad90c94ffb
janmaijay1997 <30903817+janmaijay1997@users.noreply.github.com>
2021-02-28 02:35:32 +0530

    spring security demo

---
+++ SpringSecurityDemo/src/main/java/com/intent/SpringSecurityDemo/Controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.intent.SpringSecurityDemo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	
+	@GetMapping("/")
+	public String Welcome() {
+		return "<h1>Welcome<h1>";
+	}
+}
